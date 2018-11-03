fe27d061d8f2a886f2a2538b06010e1f876d12f9
Denis Tamkovich <jaselnikpromise@gmail.com>
2018-11-03 22:52:01 +0300

    initial commit

---
+++ app/route.py
@@ -0,0 +1,25 @@
+from flask import jsonify
+
+from logic_application.blockchain import Blockchain
+from app import app
+
+blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
